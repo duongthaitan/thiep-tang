5e78a86bd3a5df0319e161e62c9356a2db70e5e9
Dương Thái Tân <96938726+TanHipp@users.noreply.github.com>
2022-10-01 00:16:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Tuyet Anh';
+const giftUrl = 'https://www.facebook.com/TanHipp';
+const eventName = 'Chúc Mừng Sinh Nhật 1-10';
+const titleCard = 'Tặng Tuyết Anh';
+const contentCard = 'Chúc bạn tôi  01/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
